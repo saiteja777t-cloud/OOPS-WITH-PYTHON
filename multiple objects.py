cc39b95419854bebd884b5f5b3a12a837f6f961b
saiteja777t-cloud <saiteja777t@gmail.com>
2026-09-07 15:51:20 +0530

    with multiple objects

---
+++ multiple objects.py
@@ -0,0 +1,11 @@
+class sample():
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def read(self):
+        print(self.name)
+        print(self.age)
+s1=sample("kprit",20)
+s1.read()
+s2=sample("abc",21)
+s2.read()
